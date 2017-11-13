039a98e819b4aeaa1fd7db60e411b3514b67911b
tbalden <illespal@gmail.com>
2017-11-13 14:09:28 -0800

    drivers: misc: implement usb fast charge mode
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Enables force charging up to 900mA in usb mode
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    (cherry picked from commit a6bc8f9bf20a50705006e4a4416b595df8bf286c)
    
    UCI fast charge config on fastcharge property name

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -26,6 +26,10 @@
 #include <linux/nanohub_htc.h>
 #include <linux/qpnp/qpnp-adc.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
@@ -1067,6 +1071,13 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 {
 	int rc;
 	u8 icl_options;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (get_force_fast_charge() && icl_ua == USBIN_500MA)
+	{
+		icl_ua = USBIN_900MA;
+	}
+#endif
 
 	/* power source is SDP */
 	switch (icl_ua) {
